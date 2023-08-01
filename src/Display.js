777051070b860c7ee4551b224ce9e8395f600c7e
palla023 <65664262+palla023@users.noreply.github.com>
2023-08-01 16:29:59 +0530

    Added Offer Ended Condition

--- src/Display.js
+++ src/Display.js
@@ -1,29 +1,35 @@
-import React from 'react'
-//If we want to use in our component ,First We have to destructure the data what we are getting from the another component 
-const Display = ({days,hours,minutes,seconds}) => {
-    return (
+import React from "react";
+//If we want to use in our component ,First We have to destructure the data what we are getting from the another component
+const Display = ({ days, hours, minutes, seconds, offerEnded }) => {
+  return (
+    <div>
+      <h1 className="mt-5 fw-bold">SALES COUNTDOWN TIMER</h1>
+      {offerEnded ? (
+        <h2 className="text-danger fst-italic fw-semibold">Offer Ended</h2>
+      ) : (
         <div>
-            <h1>SALES COUNTDOWN TIMER</h1>
-            <h2>Offer ends in</h2>
-            {/* For the proper Alignmenet of minutes and corresponding number 60 ,have a same space for both use margin-right same*/}
-            <table>
-                <tbody>
-                    <tr>
-                        <th>{days}</th>&nbsp;&nbsp;
-                        <th>{hours}</th>&nbsp;&nbsp;
-                        <th>{minutes}</th>&nbsp;&nbsp;
-                        <th>{seconds}</th>&nbsp;&nbsp;
-                    </tr>
-                    <tr>
-                        <td>DAYS</td>&nbsp;&nbsp;
-                        <td>HOURS</td>&nbsp;&nbsp;
-                        <td>MINUTES</td>&nbsp;&nbsp;
-                        <td>SECONDS</td>&nbsp;&nbsp;
-                    </tr>
-                </tbody>
-            </table>
+          <h2>Offer ends in</h2>
+          {/* For the proper Alignmenet of minutes and corresponding number 60 ,have a same space for both use margin-right same*/}
+          <table>
+            <tbody>
+              <tr className="text-center">
+                <th>{days >= 0 ? days : 0}</th>&nbsp;&nbsp;
+                <th>{hours >= 0 ? hours : 0}</th>&nbsp;&nbsp;
+                <th>{minutes >= 0 ? minutes : 0}</th>&nbsp;&nbsp;
+                <th>{seconds >= 0 ? seconds : 0}</th>&nbsp;&nbsp;
+              </tr>
+              <tr>
+                <td>DAYS</td>&nbsp;&nbsp;
+                <td>HOURS</td>&nbsp;&nbsp;
+                <td>MINUTES</td>&nbsp;&nbsp;
+                <td>SECONDS</td>&nbsp;&nbsp;
+              </tr>
+            </tbody>
+          </table>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Display
+export default Display;
