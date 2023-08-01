ecdc76e5032544f89b6851ff6b3ecfa533c532e0
palla023 <65664262+palla023@users.noreply.github.com>
2023-08-01 16:29:10 +0530

    added Offer Ended Condition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React,{useEffect,useState} from 'react'
-import Display from './Display';
+import React, { useEffect, useState } from "react";
+import Display from "./Display";
 
 const App = () => {
   //end time = offer EndTime , we will be stored in the form of milliseconds
-  const endTime = new Date('September 15, 2022 12:12:00').getTime();   //getTime() => it will return in ms for that particular Date Time
+  const endTime = new Date("August 01, 2023 16:26:00").getTime(); //getTime() => it will return in ms for that particular Date Time
   //For every Second There will be Some Changes, that changes also have to be stored , that's y we used setCurrentTime
-  const [currentTime,setcurrentTime] = useState(new Date().getTime());
+  const [currentTime, setcurrentTime] = useState(new Date().getTime());
+  const [offerEnded, setOfferEnded] = useState(false);
   const gap = endTime - currentTime; //in milliseconds => 177670892
 
   const seconds = 1000; // in milliseconds
@@ -13,35 +14,43 @@ const App = () => {
   const hours = minutes * 60;
   const days = hours * 24;
 
-  const remainingDays = Math.floor(gap / days);
-  const remainingHours = Math.floor( (gap % days) / hours);
-  const remainingMinutes = Math.floor( (gap % hours) / minutes);
-  const remainingSeconds = Math.floor( (gap % minutes) / seconds);
+  var remainingDays = Math.floor(gap / days);
+  var remainingHours = Math.floor((gap % days) / hours);
+  var remainingMinutes = Math.floor((gap % hours) / minutes);
+  var remainingSeconds = Math.floor((gap % minutes) / seconds);
 
   //If this function is not there means we have to Refresh EveryTime for the Remaining Offer time, To See live we have to Use this Function
-  //whenever the dependency variable changes, that time the component will be re-rendered 
-  useEffect(()=>{
+  //whenever the dependency variable changes, that time the component will be re-rendered
+  useEffect(() => {
     //SetTime will take 2 arg. 1=> what action tp be Performed 2=>for how many millisec you want to update the function
-    setTimeout(()=>setcurrentTime(new Date().getTime()),1000); 
-  },[currentTime]) // 11:30:55   Based on current time means , for every milliSecond the component will be reRenderd
+    setTimeout(() => setcurrentTime(new Date().getTime()), 1000);
+  }, [currentTime]); // 11:30:55   Based on current time means , for every milliSecond the component will be reRenderd
+
+  useEffect(() => {
+    const gap = endTime - currentTime;
+    setOfferEnded(gap <= 0);
+  }, [endTime, currentTime]);
 
   return (
     <div>
       <center>
         {/*Send the data by variable to the Display Component to display in our UI */}
 
-        <Display days={remainingDays} 
-        hours={remainingHours} minutes={remainingMinutes}
-        seconds={remainingSeconds} />
-
+        <Display
+          days={remainingDays}
+          hours={remainingHours}
+          minutes={remainingMinutes}
+          seconds={remainingSeconds}
+          offerEnded={offerEnded}
+        />
       </center>
     </div>
-  )
-}
+  );
+};
 export default App;
 
 
-{/*
+  /*
   const seconds = 1000; // in milliseconds
   const minutes = seconds * 60;
   const hours = minutes * 60;
@@ -60,8 +69,9 @@ export default App;
     const remainingHours = Math.floor( (gap % days) / hours);         days -> hrs  = (days <- hrs) 
     const remainingMinutes = Math.floor( (gap % hours) / minutes);    
     const remainingSeconds = Math.floor( (gap % minutes) / seconds);
-
+    console.log(remainingDays +"days " + remainingHours + "hours "+remainingMinutes  + "min "+ remainingSeconds   + " sec")
     Check for the Higher one  use % for that and use / for original
     Perform % with Higher One and / with original what you want
     hours =    (     % days )              / hours
-  */ }
+  */
+
